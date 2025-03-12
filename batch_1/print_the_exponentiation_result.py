7470326c4f4f8567c66f6d20177eb49fe328e024
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-12 11:50:25 +0800

    print the result

--- batch_1/print_the_exponentiation_result.py
+++ batch_1/print_the_exponentiation_result.py
@@ -1,3 +1,7 @@
-# input two numbers
-# raise the base to the exponent 
-# print the result
+
+num1 = float(input("Enter the first number (base):"))
+num2 = float(input("Enter the second number (exponent):"))
+
+power = float (num1) ** float(num2)
+
+print (num1 ** num2)
