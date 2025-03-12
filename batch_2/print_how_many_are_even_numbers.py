1b0f18cf8acda0103dd181f2ccea82b5c149813e
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-12 14:30:52 +0800

    print the even numbers

--- batch_2/print_how_many_are_even_numbers.py
+++ batch_2/print_how_many_are_even_numbers.py
@@ -1,3 +1,4 @@
-# input ten numbers
-# count the even number in the list
-# print the even numbers
+num = [int(input(f"Enter numbers {i+1} :")) for i in range(10)]
+
+evenum = sum(1 for n in num if n % 2 == 0)
+print("number of even:", evenum)
