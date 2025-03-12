8092eec6161b9da7e729d50a86d1b209e8cb1946
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-12 08:43:16 +0800

    print the bigger number

--- batch_1/print_the_bigger_number.py
+++ batch_1/print_the_bigger_number.py
@@ -1,5 +1,6 @@
-# input two numbers
 num1 = int(input("Enter the first number:"))
 num2 = int(input("Enter the second number:"))
-# compare the two numbers
-# print the bigger number
+if num1 > num2:
+    print(f"{num1} is the bigger number")
+elif num2 > num1:
+    print(f"{num2} is the bigger number")
