39381b0b530f05366d39c72a3e720c1fbf6ba0c2
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-13 00:23:59 +0800

    display unique or duplicate

--- batch_3/display_unique_or_duplicate.py
+++ batch_3/display_unique_or_duplicate.py
@@ -1,3 +1,13 @@
-# ask to input a number
-# check if the number is already in the list
-# stop asking for an input if value is invalid
+numbers = []
+
+while True:
+    try:
+        num = int(input("Enter a number:"))
+        if num in numbers:
+            print ("Duplicate")
+        else:
+            print ("Unique")
+        numbers.append(num)
+    except ValueError:
+        print ("Invalid")
+        break
