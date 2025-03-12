bb3d7bd46cf33fea5c412f25ff3e67a913ec4d92
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-12 13:19:03 +0800

    print the remainder

--- batch_2/print_the_remainder.py
+++ batch_2/print_the_remainder.py
@@ -1,3 +1,6 @@
-# input two numbers
-# compute the remainder when first number is divided by the second number
-# print the remainder
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+
+quotient = float(num1) % float(num2)
+
+print ("The remainder is:", num1 % num2)
