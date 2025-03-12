d21b04b92bfc2b1d729a24b05a2033cf110555c9
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-13 01:40:22 +0800

    display the numbers form lowest to highest

--- batch_3/display_the_number_from_lowest_to_highest.py
+++ batch_3/display_the_number_from_lowest_to_highest.py
@@ -1,15 +1,10 @@
-# Initialize an empty list to input numbers
 numbers = []
-# continously ask for input using while true loop
 while True:
     try:
         num = int(input("Enter a number:"))
-# check if the input is valid
-# append the valid input to the list
         numbers.append(num)
     except ValueError:
         break
-# sort the numbers from lowest to highest
-# display the sorted numbers
+
 if numbers:
     print("Sorted numbers:", sorted(numbers))
