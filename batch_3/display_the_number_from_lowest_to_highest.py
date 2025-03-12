33db2ee5f534a720563ccdeb34d7e03854c3dd8a
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-13 01:28:56 +0800

    added a pseudocode

---
+++ batch_3/display_the_number_from_lowest_to_highest.py
@@ -0,0 +1,6 @@
+# Initialize an empty list to input numbers
+# continously ask for input using while true loop
+# check if the input is valid
+# append the valid input to the list
+# sort the numbers from lowest to highest
+# display the sorted numbers
