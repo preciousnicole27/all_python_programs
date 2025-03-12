4de1ac114c8ec61c94a56803870033903eb5610c
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-12 12:45:19 +0800

    print the smaller number

--- batch_2/print_the_smaller_number.py
+++ batch_2/print_the_smaller_number.py
@@ -1,3 +1,7 @@
-# input two numbers
-# compare which is the smaller number
-# print the smaller number
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+
+if num1 < num2:
+    print(f"{num1} is the smaller number")
+elif num2 < num1:
+    print(f"{num2} is the smaller number")
