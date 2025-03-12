fbd28d26641a74ca7cbd2e094c0d4bf4f49ae551
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-13 01:13:51 +0800

    check if the input is valid

--- batch_3/display_the_lowest_number.py
+++ batch_3/display_the_lowest_number.py
@@ -1,8 +1,12 @@
 # initialize a variable to store the lowest number, intially set it to none
-lowest_num = None
+lowest_num = []
 # use loop to keep asking for an input
 while True:
-    num = input("Enter a number:")
+    try:
+        num = int(input("Enter a number:"))
+        nums.append(num)
 # check if the input is a valid number
+    except ValueError:
+        break
 # check if the number is the lowest
 # print the lowest number
