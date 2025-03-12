09c23897c9af3387605586de0f4a39e39e829f13
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-12 23:34:36 +0800

    display first entry only

--- batch_3/display_only_first_entry_for_numbers_with_duplicate.py
+++ batch_3/display_only_first_entry_for_numbers_with_duplicate.py
@@ -1,4 +1,11 @@
-# input ten numbers
-# display all the numbers
-# find numbers with duplicate
-# display the first entry of numbers with duplicate
+number = [int(input(f"Enter a number {i+1} :")) for i in range(10)]
+
+unrepeated_numbers = []
+seen = set()
+for num in number:
+    if num not in seen:
+        unrepeated_numbers.append(num)
+        seen.add(num)
+
+print("Numbers:",number)
+print("Numbers with first entry:", unrepeated_numbers)
