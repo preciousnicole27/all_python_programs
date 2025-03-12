0ba29b05b20f1909f3ff4af088369d2996fad72e
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-12 12:09:07 +0800

    print the number of odd

--- batch_1/print_the_odd_numbers.py
+++ batch_1/print_the_odd_numbers.py
@@ -1,3 +1,4 @@
-# input ten numbers
-# count the numbers of odd
-# print the number of odds
+num = [int(input(f"Enter numbers_{i+1} :")) for i in range(10)]
+oddnum = sum(1 for n in num if n % 2 != 0)
+
+print("number of odds:", oddnum)
