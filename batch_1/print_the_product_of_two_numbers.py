e1c12449a0875a8678781455e15b95feb8d3409c
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-12 10:44:19 +0800

    print the product

--- batch_1/print_the_product_of_two_numbers.py
+++ batch_1/print_the_product_of_two_numbers.py
@@ -1,3 +1,6 @@
-# input two numbers
-# multiply the two numbers
-# print the product
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+
+product= int(num1) * int(num2)
+
+print (num1 * num2)
