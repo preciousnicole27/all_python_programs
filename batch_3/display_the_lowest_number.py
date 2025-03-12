3a2d8e606b92e671dad1d8202a19d04ec834c9bc
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-13 00:40:46 +0800

    added a pseudocode

---
+++ batch_3/display_the_lowest_number.py
@@ -0,0 +1,5 @@
+# initialize a variable to store the lowest number, intially set it to none
+# use loop to keep asking for an input
+# check if the input is a valid number
+# check if the number is the lowest
+# print the lowest number
