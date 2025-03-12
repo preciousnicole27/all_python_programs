4f38085f01b61deeea761f33a561203ad0c99947
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-12 20:18:06 +0800

    display numbers that doesnt have a duplicate

--- batch_3/display_all_the_numbers_that_dont_have_duplicate.py
+++ batch_3/display_all_the_numbers_that_dont_have_duplicate.py
@@ -1,3 +1,5 @@
-# input ten numbers
-# find numbers that only appeared once
-# print numbers without duplicate
+number = [int(input(f"Enter numbers {i+1} :")) for i in range(10)]
+
+unrepeated_numbers = [num for num in number if number.count(num) == 1]
+
+print ("Numbers with no duplicates:", unrepeated_numbers)
