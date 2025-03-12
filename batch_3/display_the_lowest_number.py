4ecc29bf3b71d5aca884cae20260416e83ff7ab8
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-13 00:42:40 +0800

    initialize variable

--- batch_3/display_the_lowest_number.py
+++ batch_3/display_the_lowest_number.py
@@ -1,4 +1,5 @@
 # initialize a variable to store the lowest number, intially set it to none
+lowest_num = None
 # use loop to keep asking for an input
 # check if the input is a valid number
 # check if the number is the lowest
