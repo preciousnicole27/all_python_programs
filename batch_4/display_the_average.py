b89ff6bf4d622f7d84ddf9373dc7884c4661d663
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-13 12:56:09 +0800

    ask for user input

--- batch_4/display_the_average.py
+++ batch_4/display_the_average.py
@@ -1,5 +1,10 @@
 # initialize and empty list to store numbers
+numbers =  []
 # ask for user input continously using while true loop
+while True:
+    try:
+        num = int(input("Enter a number:"))
+        numbers.append(num)
 # check if the input is valid
 # calculate the average of the numbers in the list
 # display the average
