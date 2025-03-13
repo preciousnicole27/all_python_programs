8b64971052d97012bed3d264d0c4e457a72c315e
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-13 11:48:32 +0800

    use while true loop

--- batch_4/display_number_with_most_duplicate.py
+++ batch_4/display_number_with_most_duplicate.py
@@ -1,5 +1,8 @@
 # intialize an empty list to store numbers
+numbers = []
 # start a loop using while True
+while True:
 # ask for user input
 # check if the input is valid
 # display the number/s with the most number of duplicate
+
