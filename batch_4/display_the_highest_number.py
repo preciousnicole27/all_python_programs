c93551269a4210642ed63cd9afcc8586315b35dd
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-13 12:09:28 +0800

    print the highest number

--- batch_4/display_the_highest_number.py
+++ batch_4/display_the_highest_number.py
@@ -1,12 +1,10 @@
-# initialize an empty list to store numbers
 numbers = []
-# start a loop using while True to take user input
 while True:
     try:
         num =  int(input("Enter a number:"))
         numbers.append(num)
-# check if the input is valid
     except ValueError:
         print ("Invalid")
         break
-# display the highest number
+if numbers:
+    print("Highest number:", max(numbers))
