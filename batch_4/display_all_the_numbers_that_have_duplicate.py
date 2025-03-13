134d8231ce12b29240063dd10a33ba91e3d54efc
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-13 11:14:43 +0800

    display the number of duplicates

--- batch_4/display_all_the_numbers_that_have_duplicate.py
+++ batch_4/display_all_the_numbers_that_have_duplicate.py
@@ -1,5 +1,5 @@
 number = [int(input(f"Enter numbers {i+1} :")) for i in range(10)]
 
 duplicated_numbers = list(set([num for num in number if number.count (num) > 1]))
-# display duplicate numbers
+
 print ("Numbers with duplicates:", duplicated_numbers)
