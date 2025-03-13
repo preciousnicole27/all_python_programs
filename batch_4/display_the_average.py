cba83527749ef8f452d6d13a76b1ee042d658c51
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-13 12:54:28 +0800

    added a pseudocode

---
+++ batch_4/display_the_average.py
@@ -0,0 +1,5 @@
+# initialize and empty list to store numbers
+# ask for user input continously using while true loop
+# check if the input is valid
+# calculate the average of the numbers in the list
+# display the average
