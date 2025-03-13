b537891f92f388f01b37c549096fbab5be4dcdb0
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-13 12:44:41 +0800

    display numbers in descending order

--- batch_4/display_number_from_highest_to_lowest.py
+++ batch_4/display_number_from_highest_to_lowest.py
@@ -1,11 +1,9 @@
-# intialize an empty list to store numbers
 numbers = []
-# ask for user input continously using while true loop
 while True:
     try:
          num = int(input("Enter a number:"))
          numbers.append(num)
-# check if the input is valid
     except ValueError:
          break
-# display the numbers in descending order
+if numbers:
+     print("Sorted numbers:", sorted(numbers, reverse= True))
