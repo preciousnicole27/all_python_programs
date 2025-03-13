3712ef4161d63dfcf6a2279609fbdf8c2adbf502
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-13 12:05:20 +0800

    ask for user input then use while true loop

--- batch_4/display_the_highest_number.py
+++ batch_4/display_the_highest_number.py
@@ -1,5 +1,9 @@
 # initialize an empty list to store numbers
 numbers = []
 # start a loop using while True to take user input
+while True:
+    try:
+        num =  int(input("Enter a number:"))
+        numbers.apped(num)
 # check if the input is valid
 # display the highest number
