df384278e966e11c12a7a871b9fcbc148980cad3
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-13 11:54:04 +0800

    display the number with most number of duplicate

--- batch_4/display_number_with_most_duplicate.py
+++ batch_4/display_number_with_most_duplicate.py
@@ -1,14 +1,10 @@
-# intialize an empty list to store numbers
 numbers = []
-# start a loop using while True
 while True:
-# ask for user input
-try:
+    try:
         num = int(input("Enter a number:"))
         print ("Duplicate" if num in numbers else "Unique")
         numbers.append(num)
-# check if the input is valid
-except ValueError:
+    except ValueError:
         break
-# display the number/s with the most number of duplicate
-
+if numbers:
+    print("Most frequent duplicated number:", max(set(numbers), key=numbers.count))
