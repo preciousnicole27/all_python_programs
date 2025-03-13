811fccffd02dad3110d0eae7f26c1d7c457858e4
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-13 11:12:22 +0800

    check and list the number of duplicates in the given input

--- batch_4/display_all_the_numbers_that_have_duplicate.py
+++ batch_4/display_all_the_numbers_that_have_duplicate.py
@@ -1,5 +1,4 @@
-# initialize an empty list to store numbers
-# ask user to input 10 numbers
 number = [int(input(f"Enter numbers {i+1} :")) for i in range(10)]
 # check for duplicate numbers
+duplicated_numbers = list(set([num for num in number if number.count (num) > 1]))
 # display duplicate numbers
