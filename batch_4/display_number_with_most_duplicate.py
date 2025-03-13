5d94bb8be7ebc1ce96b5a253b723298849d4457d
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-13 11:37:32 +0800

    added a pseudocode

---
+++ batch_4/display_number_with_most_duplicate.py
@@ -0,0 +1,5 @@
+# intialize an empty list to store numbers
+# start a loop using while True
+# ask for user input
+# check if the input is valid
+# display the number/s with the most number of duplicate
