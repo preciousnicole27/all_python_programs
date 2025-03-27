7f624cfe39c79f66f1b8c86f5cf22bacee987f41
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 01:07:35 +0800

    use for loop to  check if each letter is lower case or upper case

--- batch_6/convert_text_to_uppercase_without_using_isupper_function.py
+++ batch_6/convert_text_to_uppercase_without_using_isupper_function.py
@@ -1,6 +1,8 @@
 # define the function that converts the input to uppercase letters
-def to_all_uppercase_letter(text)
+def to_all_uppercase_letter(text):
 # find  any lower case letter 
+    for char in text:
+        if "a" <= char <= "z":
 # use the return true function if there are no lowercase letters
 # ask for user input
 # print the result
