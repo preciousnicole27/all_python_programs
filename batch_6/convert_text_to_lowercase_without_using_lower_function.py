ca08b0aaff667c16ffa9538bd514d25aee6f3141
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 00:22:03 +0800

    define the function

--- batch_6/convert_text_to_lowercase_without_using_lower_function.py
+++ batch_6/convert_text_to_lowercase_without_using_lower_function.py
@@ -1,7 +1,2 @@
 # process and define the input to convert input into lowercase
-# initialize an empty string
-# check if the input is in uppercase
-# convert the input into lowercase
-# use slicing to return the string 
-# ask for the user input 
-# print the result
+def convert_to_lowercase(text):
