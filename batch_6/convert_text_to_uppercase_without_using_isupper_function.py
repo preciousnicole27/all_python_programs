9448f83e8c427cd2f8d209e6a1d65367f4d13a0c
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 01:00:01 +0800

    define the function

--- batch_6/convert_text_to_uppercase_without_using_isupper_function.py
+++ batch_6/convert_text_to_uppercase_without_using_isupper_function.py
@@ -1,4 +1,5 @@
 # define the function that converts the input to uppercase letters
+def to_all_uppercase_letter(text)
 # find  any lower case letter 
 # use the return true function if there are no lowercase letters
 # ask for user input
