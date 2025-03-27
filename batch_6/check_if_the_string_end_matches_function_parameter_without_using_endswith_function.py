cdde5793dab6816bbd616d5391f24ab6105772c9
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 02:31:48 +0800

    check length if valid

--- batch_6/check_if_the_string_end_matches_function_parameter_without_using_endswith_function.py
+++ batch_6/check_if_the_string_end_matches_function_parameter_without_using_endswith_function.py
@@ -2,4 +2,7 @@
 def check_suffix(text, suffix):
 # slice to compare last characters of the text with the suffix
 # use return
+    if len(suffix) > len(text):
+        return False
+    return text[-len(suffix):] == suffix
 # print the result
