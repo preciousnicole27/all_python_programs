ee3fe4cfcc94f5e48af8807744855d8d729470c9
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 02:24:25 +0800

    define function compare last part of string with the given suffix

--- batch_6/check_if_the_string_end_matches_function_parameter_without_using_endswith_function.py
+++ batch_6/check_if_the_string_end_matches_function_parameter_without_using_endswith_function.py
@@ -1,4 +1,5 @@
 # define a function, check if string ends with the given suffix
+def check_suffix(text, suffix):
 # slice to compare last characters of the text with the suffix
 # use return
 # print the result
