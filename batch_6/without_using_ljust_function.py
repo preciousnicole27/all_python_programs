da80f9a47a00c208f8d45c2a741d60ae7df9ed47
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 02:49:04 +0800

    added a pseudocode

---
+++ batch_6/without_using_ljust_function.py
@@ -0,0 +1,6 @@
+# define a function that will add spaces that is equal to the given width
+# check text length
+# return text
+# add spaces until desired width
+# ask for user input
+# print the justified text
