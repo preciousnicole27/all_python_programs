3f47d9956182579017f1105ea00a709738858ff6
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-27 23:03:43 +0800

    while loop checks if text starts with a prefix, this part also check prefix length and confirms character match

--- batch_6/remove_characters_at_the_beggining_without_using_removeprefix.py
+++ batch_6/remove_characters_at_the_beggining_without_using_removeprefix.py
@@ -1,6 +1,9 @@
 # process and define the input to remove prefix
 def remove_prefix(text, prefix):
 # initialize index
+    index = 0
 # check if input starts with a prefix
+    while index < len(prefix) and index < len(text) and text[index]:
+        index += 1
 # use slicing to return the string 
 # ask for user input
