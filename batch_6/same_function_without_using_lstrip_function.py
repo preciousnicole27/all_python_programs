f16e63852045b518a22c50b760cd910d814baf51
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-27 21:10:47 +0800

    used this function to replace lstrip, only spaces at the start are removed

--- batch_6/same_function_without_using_lstrip_function.py
+++ batch_6/same_function_without_using_lstrip_function.py
@@ -1,5 +1,2 @@
 # process and define the input to remove leading spaces
-# use loop to find the  first non-space character
-# initialize index
-# use slicing to return the string 
-# get the user input
+def remove_leading_spaces(text):
