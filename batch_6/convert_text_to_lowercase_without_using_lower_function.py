08ca5822338dfc80dbd901a1e4bf70d68ddeb6bd
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 00:47:59 +0800

    ask for user input, then print the result

--- batch_6/convert_text_to_lowercase_without_using_lower_function.py
+++ batch_6/convert_text_to_lowercase_without_using_lower_function.py
@@ -2,3 +2,7 @@
 def convert_to_lowercase_letters(text):
 # convert the input into lower case letters
     return text.casefold()
+# ask for the user input
+text = input("Enter an input:")
+# print the result
+print(convert_to_lowercase_letters(text))
