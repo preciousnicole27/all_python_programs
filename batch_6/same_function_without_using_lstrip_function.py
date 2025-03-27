708599fa9513afe95eeee1e640f917fc93b3719e
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-27 20:35:45 +0800

    added a pseudocode

---
+++ batch_6/same_function_without_using_lstrip_function.py
@@ -0,0 +1,5 @@
+# process and define the input to remove leading spaces
+# use loop to find the  first non-space character
+# initialize index
+# use slicing to return the string 
+# get the user input
