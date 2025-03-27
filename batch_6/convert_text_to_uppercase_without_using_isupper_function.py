fbd13a729fecc824dba77ffd47f4eab333b66732
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 00:58:04 +0800

    added a pseudocode

---
+++ batch_6/convert_text_to_uppercase_without_using_isupper_function.py
@@ -0,0 +1,5 @@
+# define the function that converts the input to uppercase letters
+# find  any lower case letter 
+# use the return true function if there are no lowercase letters
+# ask for user input
+# print the result
