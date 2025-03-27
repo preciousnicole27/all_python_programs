14b29b0f7d2de526695210e9e1e3a33751f36b79
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-27 21:29:31 +0800

    while loop moves the index function to find the first non space input

--- batch_6/same_function_without_using_lstrip_function.py
+++ batch_6/same_function_without_using_lstrip_function.py
@@ -2,3 +2,6 @@
 def remove_leading_spaces(text):
 # use loop to find the  first non-space character
     index = 0
+# initialize index
+    while index < len(text) and text[index] == " ":
+        index += 1 
