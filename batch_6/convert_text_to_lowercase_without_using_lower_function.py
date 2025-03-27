59fe4ae3febff0507b2bed1973580a6aef2bb260
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 00:02:35 +0800

    added a pseudocode

---
+++ batch_6/convert_text_to_lowercase_without_using_lower_function.py
@@ -0,0 +1,7 @@
+# process and define the input to convert input into lowercase
+# initialize an empty string
+# check if the input is in uppercase
+# convert the input into lowercase
+# use slicing to return the string 
+# ask for the user input 
+# print the result
