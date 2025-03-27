708d35199de21b48a90823535491f523a810dcbf
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 02:54:54 +0800

    check text length then add spaces until given width

--- batch_6/without_using_ljust_function.py
+++ batch_6/without_using_ljust_function.py
@@ -2,6 +2,9 @@
 def justified_text(text,width):
 # check text length
 # return text
+    if len(text) >= width:
+        return text
 # add spaces until desired width
+    return text + " " * (width - len(text))
 # ask for user input
 # print the justified text
