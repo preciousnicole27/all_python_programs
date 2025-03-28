84b92be3fa3c8dc77cd4bc3d77cd4dd97060e097
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 23:23:41 +0800

    if word else is used to replace spaces with an empty string. then ask for user input then print the title cased reult

--- batch_6/without_using_title_function.py
+++ batch_6/without_using_title_function.py
@@ -3,6 +3,10 @@ def title_cased_text(text):
 # split the string into words
     words = text.split()
 # capitalize the first letter while the rest are in lowercase
+    result = [word [0].upper()+ word[1:].lower() if word else "" for word in words]
 # combine the words back into a string
+    return " ".join(result)
 # ask for user input
-# print the titlecasedgit
+text = input("Enter a text:")
+# print the titlecased input 
+print ("Title Case:", title_cased_text(text))
