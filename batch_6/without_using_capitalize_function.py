15aa88904e856ba7b6e2977282753f389dc3aa0e
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 21:50:28 +0800

    ask for user input, then print the capitalized input

--- batch_6/without_using_capitalize_function.py
+++ batch_6/without_using_capitalize_function.py
@@ -5,6 +5,8 @@ def capitalize_text(text):
     if not text:
         return text
 # make the first letter capital while the others are in lowercase
-    return text[0].upper() + text[1:].lower
+    return text[0].upper() + text[1:].lower()
 # ask for user input
+text = input("Enter a text:")
 # print the result
+print("Capitalized:",capitalize_text(text))
