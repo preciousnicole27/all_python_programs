c1b1274b8ea321b6ffade81610a53f072b048de4
Precious Nicole Dela Cruz <preciousnicoledelacruz.27@gmail.com>
2025-03-28 11:46:25 +0800

    Delete convert_text_to_lowercase_without_using_lower_function.py

--- convert_text_to_lowercase_without_using_lower_function.py
+++
@@ -1,8 +0,0 @@
-# define the function that converts the input to lowercase letters
-def convert_to_lowercase_letters(text):
-# convert the input into lower case letters
-    return text.casefold()
-# ask for the user input
-text = input("Enter an input:")
-# print the result
-print(convert_to_lowercase_letters(text))
