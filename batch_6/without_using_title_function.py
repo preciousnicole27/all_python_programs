d570b5bb1ac7468419671daf8f6c8be272615af1
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 23:14:55 +0800

    define the function

--- batch_6/without_using_title_function.py
+++ batch_6/without_using_title_function.py
@@ -1,6 +1,7 @@
 # define function that will turn input to title case
+def title_cased_text(text):
 # split the string into words
 # capitalize the first letter while the rest are in lowercase
 # combine the words back into a string
 # ask for user input
-# print the titlecased
+# print the titlecasedgit
