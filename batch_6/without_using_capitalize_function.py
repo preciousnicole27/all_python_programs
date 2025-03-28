625e9cd9701727cd78cf849804c2d9eadbcf8ca1
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 21:39:50 +0800

    added a pseudocode

---
+++ batch_6/without_using_capitalize_function.py
@@ -0,0 +1,6 @@
+# define function that capitalize the first letter of the string
+# check if string is empty
+# then return text if not
+# make the first letter capital while the others are in lowercase
+# ask for user input
+# print the result
