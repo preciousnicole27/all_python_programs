705d8875884c374fbe64c3b1f9a8817e1999b6b2
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 11:22:46 +0800

    counts the spaces to center the text

---
+++ without_using_center_function.py
@@ -0,0 +1,11 @@
+# define function to center text
+def center_text(text, width):
+# check text length
+# calculate spaces needed
+    total_space = max(0,width-len(text))
+    right_spaces = total_space - left_spaces
+# divide spaces equally
+    left_spaces = total_space // 2
+# return text
+# ask for user input
+# print the centered text
