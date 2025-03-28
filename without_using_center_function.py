54cb968af34f0e85de6c1c5d459b71f9376d6432
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 11:41:41 +0800

    define function

--- without_using_center_function.py
+++ without_using_center_function.py
@@ -2,10 +2,6 @@
 def center_text(text, width):
 # check text length
 # calculate spaces needed
-    total_space = max(0,width-len(text))
-    right_spaces = total_space - left_spaces
-# divide spaces equally
-    left_spaces = total_space // 2
 # return text
 # ask for user input
 # print the centered text
