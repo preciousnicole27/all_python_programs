2e21e30f7deaf54d0d2624200f75398509fcb2ed
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 14:13:54 +0800

    check first if text is upper or lowercase then if character is in lower case convert it into uppercase vice versa

--- batch_6/without_using_swapcase_function.py
+++ batch_6/without_using_swapcase_function.py
@@ -3,5 +3,7 @@ def swap_case(text):
 # create an empty string
     result = ""
 # use loop to go through each character in text to convert and swap cases
+    for char in text:
+        result += char.upper() if char.islower() else char.lower()
 # return the swapped case string
 # ask for user input then print results
