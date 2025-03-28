9aeab652d281fcd076a869e12709cb23f94422b7
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 23:16:46 +0800

    split the string to convert into words

--- batch_6/without_using_title_function.py
+++ batch_6/without_using_title_function.py
@@ -1,6 +1,7 @@
 # define function that will turn input to title case
 def title_cased_text(text):
 # split the string into words
+    words = text.split()
 # capitalize the first letter while the rest are in lowercase
 # combine the words back into a string
 # ask for user input
