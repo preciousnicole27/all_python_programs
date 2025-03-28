a9dd3c6df4e64047015851f0b05dc5c2510541e7
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 14:10:27 +0800

    add an empty string to store input

--- batch_6/without_using_swapcase_function.py
+++ batch_6/without_using_swapcase_function.py
@@ -1,6 +1,7 @@
 # define a function for swapcase
 def swap_case(text):
 # create an empty string
+    result = ""
 # use loop to go through each character in text to convert and swap cases
 # return the swapped case string
 # ask for user input then print results
