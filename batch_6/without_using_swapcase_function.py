9155c134ad11d21e9252fe60bd9778580bebf3f3
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 14:07:49 +0800

    added a pseudocode

---
+++ batch_6/without_using_swapcase_function.py
@@ -0,0 +1,5 @@
+# define a function for swapcase
+# create an empty string
+# use loop to go through each character in text to convert and swap cases
+# return the swapped case string
+# ask for user input then print results
