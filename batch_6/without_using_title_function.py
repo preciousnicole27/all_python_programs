f861a053c51f44bea16b2c2d72c9a3664a8080cc
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 23:12:04 +0800

    added a pseudocode

---
+++ batch_6/without_using_title_function.py
@@ -0,0 +1,6 @@
+# define function that will turn input to title case
+# split the string into words
+# capitalize the first letter while the rest are in lowercase
+# combine the words back into a string
+# ask for user input
+# print the titlecased
