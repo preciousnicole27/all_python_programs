2124f7097ab79fd9e8317d8a6af34bff0441174c
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-03-28 14:08:49 +0800

    define the function

--- batch_6/without_using_swapcase_function.py
+++ batch_6/without_using_swapcase_function.py
@@ -1,4 +1,5 @@
 # define a function for swapcase
+def swap_case(text):
 # create an empty string
 # use loop to go through each character in text to convert and swap cases
 # return the swapped case string
